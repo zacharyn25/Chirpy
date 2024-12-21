cb24d507cef548e5747872c19374b6fa96e8831b
Zachary Neeld <zacharyn25@gmail.com>
2024-12-21 09:12:37 -0500

    Update to check server health

--- main.go
+++ main.go
@@ -5,15 +5,26 @@ import(
 	"log"
 )
 
+func ServerHealthHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
 func main() {
+	//Instantiation 
 	mux := http.NewServeMux()
     server := http.Server{
         Addr:    ":8080",
         Handler: mux,
     }
-	
-	mux.Handle("/", http.FileServer(http.Dir(".")))
 
+	//Check if the server is ready
+	mux.HandleFunc("/healthz", ServerHealthHandler)
+	
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	//Run the server
     if err := server.ListenAndServe(); err != nil {
         log.Fatal(err)
     }
